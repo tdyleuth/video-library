f05acf13b198a968c6399ee35de341741c3f59ba
tdyleuth <tony.dyleuth@gmail.com>
2020-12-13 18:58:33 -0800

    Updated validation message in Signup form for user that already exists

--- client/src/components/Signup/Signup.jsx
+++ client/src/components/Signup/Signup.jsx
@@ -13,6 +13,7 @@ const Signup = ({
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [passwordToggle, setPasswordToggle] = useState(false);
+    const [validationMessage, setValidationMessage] = useState(false);
 
     const handleEmailInputChanges = (e) => {
         setEmail(e.target.value);
@@ -28,6 +29,7 @@ const Signup = ({
 
         try {
             const response = await api.signup(payload);
+            console.log('2323', response);
 
             if (response.data.success === true) {
                 const { token } = response.data;
@@ -44,6 +46,10 @@ const Signup = ({
             }, 2400);
         } catch (err) {
             console.log(err);
+            setValidationMessage(true);
+            setTimeout(() => {
+                setValidationMessage(false);
+            }, 4000);
         }
     };
 
@@ -93,7 +99,13 @@ const Signup = ({
                     <label className='password-label'>
                         <b>Create Password: </b>
                     </label>
-                    <div className='pass-wrapper'>
+                    <div
+                        className={
+                            validationMessage
+                                ? 'pass-wrapper shake'
+                                : 'pass-wrapper'
+                        }
+                    >
                         <input
                             onChange={handlePasswordInputChanges}
                             type={passwordToggle ? 'text' : 'password'}
@@ -107,6 +119,17 @@ const Signup = ({
                             onClick={handlePasswordToggle}
                         ></i>
                     </div>
+                    {validationMessage ? (
+                        <p id='show-validation-message'>
+                            The email you entered already exists. Please try
+                            again!
+                        </p>
+                    ) : (
+                        <p id='hide-validation-message'>
+                            The email you entered already exists. Please try
+                            again!
+                        </p>
+                    )}
                     <div className='button-container'>
                         <button className='form-signup-button'>
                             Create Account
