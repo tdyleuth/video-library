232af88d4bf866c5a56685f544aafc1d53874604
tdyleuth <tony.dyleuth@gmail.com>
2021-01-08 17:39:58 -0800

    Update

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //Connect to MongoDB
 db.on('error', console.error.bind(console, 'Mongo connection error:'));
 
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.use('/api', videoRouter, userRouter);
 
